221ec1b1bca8840b1fb2049c369802da4268d7e8
Haraldo Filho <haraldo.filho@gmail.com>
2020-08-12 08:35:01 -0300

    Edited 'map/exhibits/index.html' to dinamically create page using javascript

---
+++ map/exhibits/js/galleries.js
@@ -0,0 +1,6 @@
+var galleries = [
+  [[23.740163, 38.002117], 'Blank Wall Gallery', 'http://www.blankwallgallery.com'],
+  [[19.066552, 47.482346], 'PH21 Gallery', 'https://www.ph21gallery.com'],
+  [[24.019707, 35.518508], 'Cip Festival', 'http://www.cipfestival.com'],
+  [[-4.257888, 55.865262], 'Glasgow Photo Gallery', 'https://www.glasgowgalleryofphotography.com']
+]
