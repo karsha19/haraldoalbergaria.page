57e0dccc4c111046e374d178727feb7f2369e6ed
Haraldo Filho <haraldo.filho@gmail.com>
2020-08-28 16:05:34 -0300

    Changed countries order on panel

--- map/countries.py
+++ map/countries.py
@@ -1,9 +1,9 @@
 countries_dict = {
-  'HR': ['Croatia', 184, 243],
-  'IT': ['Italy', 76, 125],
-  'PY': ['Paraguay', 1, 1],
-  'VA': ['Vatican City', 22, 31],
-  'US': ['United States of America', 51, 132],
   'BR': ['Brazil', 176, 395],
-  'AR': ['Argentina', 20, 31]
+  'US': ['United States of America', 51, 132],
+  'AR': ['Argentina', 20, 31],
+  'PY': ['Paraguay', 1, 1],
+  'IT': ['Italy', 76, 125],
+  'VA': ['Vatican City', 22, 31],
+  'HR': ['Croatia', 184, 243]
 }
