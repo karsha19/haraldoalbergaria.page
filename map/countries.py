5378eb69ea3e783fbf5111d1446fff063d8135ce
Haraldo Filho <haraldo.filho@gmail.com>
2020-09-20 23:23:51 -0300

    Updated map

--- map/countries.py
+++ map/countries.py
@@ -1,5 +1,5 @@
 countries_dict = {
-  'BR': ['Brazil', 176, 395],
+  'BR': ['Brazil', 174, 392],
   'US': ['United States of America', 51, 132],
   'AR': ['Argentina', 20, 31],
   'PY': ['Paraguay', 1, 1],
