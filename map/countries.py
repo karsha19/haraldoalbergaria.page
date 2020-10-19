6279515693578d19b022185572352469df5786fe
Haraldo Albergaria <hpfilho@ymail.com>
2020-10-19 12:50:53 -0300

    Updated map

--- map/countries.py
+++ map/countries.py
@@ -1,9 +1,9 @@
 countries_dict = {
-  'BR': ['Brazil', 174, 392],
-  'US': ['United States of America', 51, 132],
-  'AR': ['Argentina', 20, 31],
+  'HR': ['Croatia', 184, 243],
   'PY': ['Paraguay', 1, 1],
+  'BR': ['Brazil', 174, 392],
+  'AR': ['Argentina', 20, 31],
   'IT': ['Italy', 76, 125],
-  'VA': ['Vatican City', 22, 31],
-  'HR': ['Croatia', 184, 243]
+  'US': ['United States of America', 50, 132],
+  'VA': ['Vatican City', 22, 31]
 }
