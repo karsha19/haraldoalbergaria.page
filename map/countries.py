a0dd0a87c0fef4d070ffedcf970504f673dc9f23
Haraldo Albergaria <hpfilho@ymail.com>
2021-01-18 01:02:46 -0300

    Updated Flickr Photos Map

--- map/countries.py
+++ map/countries.py
@@ -1,9 +1,9 @@
 countries_dict = {
-  'AR': ['Argentina', 20, 31],
-  'HR': ['Croatia', 184, 243],
-  'BR': ['Brazil', 171, 387],
   'IT': ['Italy', 77, 126],
+  'BR': ['Brazil', 171, 387],
+  'AR': ['Argentina', 20, 31],
   'PY': ['Paraguay', 5, 7],
+  'HR': ['Croatia', 184, 243],
   'US': ['United States of America', 50, 132],
   'VA': ['Vatican City', 22, 31]
 }
