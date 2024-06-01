a2085efc24a9bbb4311bdb021ea7fb125681f8d2
Haraldo Filho <haraldo.filho@gmail.com>
2024-06-01 16:46:32 -0300

    Added gear and attributions to dynamic data

---
+++ data/gear.js
@@ -0,0 +1,19 @@
+var cam_body = [
+  'Canon EOS Rebel T5i'
+]
+
+var zoom_lens = [
+  "Canon EF-S 10-18mm f/4.5-5.6 IS STM",
+  "Canon EF-S 18-55mm f/3.5-5.6 IS STM",
+  "Canon EF-S 55-250mm f/4-5.6 IS STM"
+]
+
+var prime_lens = [
+  "Canon EF-S 24mm f/2.8 STM",
+  "Canon EF 50mm f/1.8 STM"
+]
+
+var teleconverter = [
+  "Kenko TELEPLUS HD DGX 1.4x",
+  "Kenko TELEPLUS HD DGX 2x"
+]
