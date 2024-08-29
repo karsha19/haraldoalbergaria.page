7759bea3b3c1fcabc7f65515870044474717f78f
HaraldoFilho <haraldo.filho@gmail.com>
2024-08-29 02:04:13 -0300

    [auto] Updated Flickr Photos Map

--- map/countries.py
+++ map/countries.py
@@ -1,11 +1,11 @@
 countries = {
-  'UY': ['Uruguay', 36, 67],
+  'HR': ['Croatia', 184, 243],
   'VA': ['Vatican City', 22, 31],
+  'PY': ['Paraguay', 1, 1],
+  'UY': ['Uruguay', 36, 67],
+  'CL': ['Chile', 78, 198],
   'IT': ['Italy', 77, 126],
   'BR': ['Brazil', 177, 395],
-  'AR': ['Argentina', 61, 131],
-  'CL': ['Chile', 78, 198],
-  'PY': ['Paraguay', 1, 1],
-  'HR': ['Croatia', 184, 243],
-  'US': ['United States of America', 50, 132]
+  'US': ['United States of America', 50, 132],
+  'AR': ['Argentina', 61, 131]
 }
